abf6c7e770e7d8fb6f4cb445025f8f5986f4c75e
Masaki Ikegami <salvage0707@gmail.com>
2018-04-10 18:47:25 +0900

    finish make-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
 	root 'post_images#index'
 
   devise_for :users
 
-  resources :post_images, only:[:new, :create, :index, :show] do
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
   	resource :post_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
 end
